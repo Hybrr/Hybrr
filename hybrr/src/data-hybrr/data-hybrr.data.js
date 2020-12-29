b5330b6c9aee5d8ce60a5405110c656d64cc82a3
Avish Shah <avishshah1108@gmail.com>
2020-12-29 18:25:21 +0530

    adding about us component

--- hybrr/src/data-hybrr/data-hybrr.data.js
+++ hybrr/src/data-hybrr/data-hybrr.data.js
@@ -1,22 +1,22 @@
 const HOMEPAGE_DATA = [
     {
         id: 1,
-        title: 'Annual maintenance charges',
+        title: 'Annual Maintenance Charges',
         content: 'Our development teams keep your software applications managed and maintained following the industry best practices and make sure they run effortlessly over the period of time.'
     },
     {
         id: 2,
-        title: 'Software development',
+        title: 'Software Development',
         content: 'We offered conceiving and specifying programming and creating maintaining applications, frameworks or other software components for major operating systems(windows and Linux).'
     },
     {
         id: 3,
-        title: 'Digital marketing',
+        title: 'Digital Marketing',
         content: 'Step towards digitalization with us.Hyberr encompasses all marketing efforts that use an electronic device or the internet.Businesses leverage digital channels such as search engines, social media, email, and other websites to connect with current and prospective customers.We provide innovative solutions at every step in the marketing journey.'
     },
     {
         id: 4,
-        title: 'Application development',
+        title: 'Application Development',
         content: 'We offer the best app development solutions for mobile apps. We leverage native features for ios, android and windows for cross-platforms development.'
     },
     {
@@ -32,17 +32,17 @@ const HOMEPAGE_DATA = [
     },
     {
         id: 7,
-        title: 'Customize software',
+        title: 'Customize Software',
         content: 'Willing to get your application designed with best user interface? We are having the solutions for all your problems.'
     },
     {
         id: 8,
-        title: 'Cyber security',
+        title: 'Cyber Security',
         content: "Reduce complexity with integrated innovations.And Don't get hooked by phishers.Cyber security protects the data and integrity of computing assets belonging to or connecting to an organization's network.Its purpose is to defend those assets against all threat actors throughout the entire life cycle of a cyber-attack"
     },
     {
         id: 9,
-        title: 'College projects',
+        title: 'College Projects',
         content:
             'For students and hobbyists, we picked out some computer engineering projects from all over the internet. These are simple and interesting hardware and software development projects while providing a solution that could come in handy in real life scenarios and can be easily used in the Final year Project.'
     }
