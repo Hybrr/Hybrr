5afd1e483fdfb3ebcab4a8b584c6f8a8580777cd
Avish Shah <avishshah1108@gmail.com>
2020-12-29 16:36:25 +0530

    facebook link added

--- hybrr/src/component/header/header.component.jsx
+++ hybrr/src/component/header/header.component.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 
 import { Link, NavLink, Route } from 'react-router-dom';
 
-import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { ReactComponent as Logo} from '../../assets/logo.svg';
+import { ReactComponent as Fblogo} from '../../assets/fblogo.svg';
 import { ReactComponent as Inst } from '../../assets/inst.svg';
 
 import './header.styles.scss';
 
 const Header = () => (
     <div className = 'header'>
-        <Logo className = 'logo'/>
-        <Inst className = 'inst'/>
-    </div>
+        <Logo className = 'header-logo'/>
+        <div className = 'logo'>
+            <a href = "https://www.instagram.com/hybrr_/">
+                <Inst className = 'inst'/>
+            </a>
+            <a href = "https://www.facebook.com/Hybrr-108302441072334">
+                <Fblogo className = 'fblogo'/>
+            </a>
+        </div>
+     </div>
     
 );
 
