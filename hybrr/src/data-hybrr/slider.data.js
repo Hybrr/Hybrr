22f025e48411137a293b5ffb8d033a8d7afb11ba
Avish Shah <avishshah1108@gmail.com>
2020-12-31 02:43:56 +0530

    styling website

--- hybrr/src/data-hybrr/slider.data.js
+++ hybrr/src/data-hybrr/slider.data.js
@@ -4,6 +4,12 @@ const SLIDER_DATA = [
     },
     {
         image: 'https://specials-images.forbesimg.com/imageserve/5e180c9d735f8c0007998956/960x0.jpg?fit=scale'
+    },
+    {
+        image: 'https://boostlabs.com/wp-content/uploads/2019/11/data-viz-programming-languages-1-1024x682.png'
+    },
+    {
+        image: 'https://assets.entrepreneur.com/content/3x2/2000/20191112054904-FotoJet32.jpeg'
     }
 ];
 
