f21d4fa8384aa95794685093f7fb07bc7972e301
Avish Shah <avishshah1108@gmail.com>
2021-01-01 21:13:36 +0530

    pricing data styling

--- hybrr/src/data-hybrr/pricing.data.js
+++ hybrr/src/data-hybrr/pricing.data.js
@@ -1,23 +1,40 @@
 const PRICING_DATA = [
     {
         id: 1,
-        type: 'STARTER (For Small Business)',
+        type: [['STARTER'], <br></br>,
+        ['(For Small Business)']],
         price: 'Rs 6,000 (Including Tax)',
-        service: 'Bring your Business Online,Static Website Develpment,Server,Domain,Digital Marketing',
+        service: [['Bring your Business Online'], <br></br>,
+        ['Static Website Develpment'], <br></br>,
+        ['Server'], <br></br>,
+        ['Domain'], <br></br>,
+        ['Digital Marketing']],
         period: 'For 7 days'
     },
     {
         id: 2,
-        type: 'HATCHLING (For Medium Business)',
+        type: [['HATCHLING'], <br></br>,
+        ['(For Medium Business)']],
         price: 'Rs 15,000 (Including Tax)',
-        service: 'Static Website Develpment,Server,Domain,Digital Marketing,Static Android Application',
+        service: [['Static Website Develpment'], <br></br>,
+        ['Server'], <br></br>,
+        ['Domain'], <br></br>,
+        ['Digital Marketing'], <br></br>,
+        ['Static Android Application']],
         period: 'For 14 days'
     },
     {
         id: 3,
-        type: 'BUSINESS (For Larger Business)',
+        type: [['BUSINESS'], <br></br>,
+        ['(For Larger Business)']],
         price: 'Rs 25,000 (Including Tax)',
-        service: 'Dynamic Website Develpment,Server,Domain,Digital Marketing,Static Android Application,Secure your website,AES DES',
+        service: [['Dynamic Website Develpment'], <br></br>,
+        ['Server'], <br></br>,
+        ['Domain'], <br></br>,
+        ['Digital Marketing'], <br></br>,
+        ['Static Android Application'], <br></br>,
+        ['Secure your website'], <br></br>,
+        ['AES DES']],
         period: 'For 25 days'
     }
 ];
